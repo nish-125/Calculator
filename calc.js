14fde6183e64129135e6aa8072870f35f6278029
nish-125 <21311a1253@sreenidhi.edu.in>
2024-06-11 21:26:26 +0530

    Create calc.js

---
+++ calc.js
@@ -0,0 +1,13 @@
+function clearScreen() {
+document.getElementById("result").value = "";
+}
+
+function calculate() {
+var p = document.getElementById("result").value;
+var q = eval(p);
+document.getElementById("result").value = q;
+}
+
+function display(value) {
+document.getElementById("result").value += value;
+}
